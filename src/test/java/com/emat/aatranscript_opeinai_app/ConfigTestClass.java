03dae787bc317c974333ae7777db3e8b68c8417f
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-06-10 17:52:11 +0200

    test Clear openAi insturctions to get different formats answears

---
+++ src/test/java/com/emat/aatranscript_opeinai_app/ConfigTestClass.java
@@ -0,0 +1,30 @@
+package com.emat.aatranscript_opeinai_app;
+
+import com.emat.aatranscript_opeinai_app.global.OpeApiParams;
+import com.emat.aatranscript_opeinai_app.global.OpenApiConfiguration;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
+
+@Configuration
+@PropertySource("classpath:application-test.properties")
+public class ConfigTestClass {
+
+    @Value("${spring-opeanai-test-key}")
+    private String apiKey;
+
+    @Bean
+    public OpenAiChatModel openAiChatModel() {
+        OpenAiApi openAiApi = new OpenAiApi(apiKey);
+        OpenAiChatOptions openAiChatOptions = new OpenAiChatOptions();
+        openAiChatOptions.setModel(OpenAiApi.ChatModel.GPT_4_O.value);
+        OpenAiChatModel openAiChatModel = new OpenAiChatModel(openAiApi, openAiChatOptions);
+        // Tutaj zdefiniuj instancję OpenAiChatModel dla testów
+        return openAiChatModel;
+    }
+}
---
+++ src/test/java/com/emat/aatranscript_opeinai_app/clear_instructions/BaseTest.java
@@ -0,0 +1,29 @@
+package com.emat.aatranscript_opeinai_app.clear_instructions;
+
+import com.emat.aatranscript_opeinai_app.ConfigTestClass;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@PropertySource("classpath:application-test.properties")
+@ActiveProfiles("test")
+
+@ContextConfiguration(classes = ConfigTestClass.class)
+public class BaseTest {
+    @Autowired
+    OpenAiChatModel openAiChatClient;
+
+    String chat(String prompt) {
+        PromptTemplate promptTemplate = new PromptTemplate(prompt);
+        Prompt promptToSend = promptTemplate.create();
+
+        return openAiChatClient.call(promptToSend).getResult().getOutput().getContent();
+    }
+}
