d5f99a0ee791a377aa9a8810e3c09e3c967edaf4
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-06-13 16:41:45 +0200

    add configuration to vector store and rag prompt to create embeddings

--- src/main/java/com/emat/aatranscript_opeinai_app/global/VectorStoreConfig.java
+++ src/main/java/com/emat/aatranscript_opeinai_app/global/VectorStoreConfig.java
@@ -2,13 +2,13 @@ package com.emat.aatranscript_opeinai_app.global;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ai.embedding.EmbeddingModel;
 
 import java.io.File;
 import java.util.List;
@@ -21,7 +21,7 @@ public class VectorStoreConfig {
     @Bean
     SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingmodel, VectorStoreProperties vectorStoreProperties) {
         var store = new SimpleVectorStore(embeddingmodel);
-        File file = new File(vectorStoreProperties.getVectorStorePath());
+        File file = new File(vectorStoreProperties.getVectorStorePath() );
         if(file.exists()) {
             store.load(file);
             log.info("Loaded vector store from file: {} ", file.getAbsolutePath());
@@ -33,10 +33,12 @@ public class VectorStoreConfig {
                 List<Document> documentList = documentReader.get();
                 TextSplitter textSplitter = new TokenTextSplitter();
                 List<Document> splitDocs = textSplitter.apply(documentList);
+                log.info("Split {} documents into {} tokens", documentList.size(), splitDocs.size());
                 store.add(splitDocs);
             });
 
             store.save(file);
+            log.info("Saved vector store to file: {} ", file.getAbsolutePath());
         }
 
         return store;
