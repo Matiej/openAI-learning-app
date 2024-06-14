6b416f88bce57bf91a1074449c8dfa48f15531d6
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-06-14 17:25:27 +0200

    implement truck

--- src/main/java/com/emat/aatranscript_opeinai_app/global/vecotrprops/DocumentsToLoad.java
+++ src/main/java/com/emat/aatranscript_opeinai_app/global/vecotrprops/DocumentsToLoad.java
@@ -1,2 +1,24 @@
-package com.emat.aatranscript_opeinai_app.global.vecotrprops;public class DocumentsToLoad {
+package com.emat.aatranscript_opeinai_app.global.vecotrprops;
+
+import org.springframework.core.io.Resource;
+
+public class DocumentsToLoad {
+    private Resource transcript;
+    private TruckAdvisor truckadvisor;
+
+    public Resource getTranscript() {
+        return transcript;
+    }
+
+    public void setTranscript(Resource transcript) {
+        this.transcript = transcript;
+    }
+
+    public TruckAdvisor getTruckadvisor() {
+        return truckadvisor;
+    }
+
+    public void setTruckadvisor(TruckAdvisor truckadvisor) {
+        this.truckadvisor = truckadvisor;
+    }
 }
--- src/main/java/com/emat/aatranscript_opeinai_app/truckadvisor/services/TruckAdvServiceImpl.java
+++ src/main/java/com/emat/aatranscript_opeinai_app/truckadvisor/services/TruckAdvServiceImpl.java
@@ -9,11 +9,15 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -22,6 +26,7 @@ import java.util.Map;
 public class TruckAdvServiceImpl implements TruckAdvService {
     private final String LANGUAGE_PROMPT = "answer using the same language in which the question was asked.";
     private final ChatClientFactory chatClientFactory;
+    private final VectorStore vectorStore;
 
     @Value("classpath:templates/rag-truck-advisor-prompt.st")
     private Resource truckAdvisorPrompt;
@@ -41,5 +46,10 @@ public class TruckAdvServiceImpl implements TruckAdvService {
         String chatOutput = chatResponse.getResult().getOutput().getContent();
         log.info("Received response from OpenAI: {}", chatOutput);
         return new TruckAdvAnswer(chatOutput);
+    }
+
+    private List<String> getContent(String question) {
+        List<Document> documentList = vectorStore.similaritySearch(SearchRequest.query(question).withTopK(4));
+        return documentList.stream().map(Document::getContent).toList();
     }
 }
