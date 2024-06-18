7998c51fbf4e3deb01bfbbcabf679911798b94c1
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-06-18 12:01:36 +0200

    implement function to run ninja api request

--- src/main/java/com/emat/aatranscript_opeinai_app/weather/function/WeatherServiceFunction.java
+++ src/main/java/com/emat/aatranscript_opeinai_app/weather/function/WeatherServiceFunction.java
@@ -1,2 +1,24 @@
-package com.emat.aatranscript_opeinai_app.weather.function;public class WeatherServiceFunction {
+package com.emat.aatranscript_opeinai_app.weather.function;
+
+import com.emat.aatranscript_opeinai_app.weather.model.WeatherRequest;
+import com.emat.aatranscript_opeinai_app.weather.model.WeatherResponse;
+import com.emat.aatranscript_opeinai_app.weather.ninja_weather_api.NinjaWeatherApiService;
+import com.emat.aatranscript_opeinai_app.weather.services.WeatherAIService;
+import org.springframework.ai.evaluation.EvaluationRequest;
+
+import java.util.function.Function;
+
+public class WeatherServiceFunction implements Function<WeatherRequest, WeatherResponse> {
+    private final NinjaWeatherApiService ninjaWeatherApiService;
+
+    public WeatherServiceFunction(NinjaWeatherApiService ninjaWeatherApiService) {
+        this.ninjaWeatherApiService = ninjaWeatherApiService;
+    }
+
+
+    @Override
+    public WeatherResponse apply(WeatherRequest request) {
+        WeatherResponse weatherResponse = ninjaWeatherApiService.getWeather(request);
+        return weatherResponse;
+    }
 }
