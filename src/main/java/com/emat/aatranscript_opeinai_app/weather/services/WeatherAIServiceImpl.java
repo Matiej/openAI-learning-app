cc8fa3b0a730da64936bbf80a90cd840763e7d8f
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-06-18 15:41:50 +0200

    refactor to use templates

--- src/main/java/com/emat/aatranscript_opeinai_app/weather/services/WeatherAIServiceImpl.java
+++ src/main/java/com/emat/aatranscript_opeinai_app/weather/services/WeatherAIServiceImpl.java
@@ -1,6 +1,7 @@
 package com.emat.aatranscript_opeinai_app.weather.services;
 
 import com.emat.aatranscript_opeinai_app.transcription.services.ChatClientFactory;
+import com.emat.aatranscript_opeinai_app.weather.function.WeatherServiceFunction;
 import com.emat.aatranscript_opeinai_app.weather.model.WeatherAnswer;
 import com.emat.aatranscript_opeinai_app.weather.model.WeatherRequest;
 import com.emat.aatranscript_opeinai_app.weather.model.WeatherResponse;
@@ -8,50 +9,63 @@ import com.emat.aatranscript_opeinai_app.weather.ninja_weather_api.NinjaWeatherA
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.model.function.FunctionCallbackWrapper;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class WeatherAIServiceImpl implements WeatherAIService {
-    private final String LANGUAGE_PROMPT = "answer using the same language in which the question was asked. Use only Polish or English, by default the answer will be in English.";
     private final ChatClientFactory chatClientFactory;
     private final NinjaWeatherApiService ninjaWeatherApiService;
 
     @Value("classpath:templates/weather-user-prompt.st")
     private Resource weatherUserPromptResource;
 
+    @Value("classpath:templates/weather-system-prompt.st")
+    private Resource systemPromptResource;
+
     @Override
     public WeatherAnswer getWeatherBasicAnswer(String city) {
         OpenAiApi.ChatModel gpt4 = OpenAiApi.ChatModel.GPT_4;
         ChatClient client = chatClientFactory.createClient(gpt4);
         log.info("Asking weather for the city: {}, using model: {}",city, gpt4);
-        PromptTemplate userPromptTemplate = new PromptTemplate(weatherUserPromptResource);
-        Prompt userPrompt = userPromptTemplate.create(Map.of("city", city));
 
-        WeatherResponse weatherFromApi = getWeatherFromApi(new WeatherRequest(city, null, null));
+        var promptOptions = OpenAiChatOptions.builder()
+                .withFunctionCallbacks(List.of(FunctionCallbackWrapper.builder(new WeatherServiceFunction(ninjaWeatherApiService))
+                        .withName("CurrentWeather")
+                        .withDescription("Get the current weather for a location").build()))
+                .build();
+        List<Message> messages = new ArrayList<>();
+        messages.add(new SystemPromptTemplate(systemPromptResource).createMessage());
+        messages.addAll(new ArrayList<>(new PromptTemplate(weatherUserPromptResource).create(Map.of("city", city)).getInstructions()));
 
-//        ChatResponse chatResponse = client.prompt()
-//                .system(LANGUAGE_PROMPT)
-//                .messages(userPrompt.getInstructions())
-//                .call()
-//                .chatResponse();
-//        String chatOutput = chatResponse.getResult().getOutput().getContent();
-//        log.info("Received response from OpenAI: {}", chatOutput);
-//        return new WeatherAnswer(chatOutput);
-        return new WeatherAnswer("Temporary answer from WeatherAIServiceImpl.");
+        Prompt prompt = new Prompt(messages, promptOptions);
+
+        ChatResponse chatResponse = client.prompt(prompt)
+                .call().chatResponse();
+        String chatOutput = chatResponse.getResult().getOutput().getContent();
+        log.info("Received chat response: {}", chatOutput);
+        return new WeatherAnswer(chatOutput);
     }
 
     private WeatherResponse getWeatherFromApi(WeatherRequest request) {
-        WeatherResponse weatherResponse = ninjaWeatherApiService.getWeather(request);
-        log.info("Received weather from Ninja API: {} for the city: {}", weatherResponse, request.location());
-        return weatherResponse;
+        WeatherServiceFunction weatherServiceFunction = new WeatherServiceFunction(ninjaWeatherApiService);
+        WeatherResponse response = weatherServiceFunction.apply(request);
+        log.info("Received weather from Ninja API: {} for the city: {}", response, request.location());
+        return response;
     }
 }
